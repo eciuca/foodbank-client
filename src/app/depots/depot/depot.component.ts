200543b1566efdd5da69db4d56cae67058f669e9
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-03-11 01:43:09 +0200

    Implement Save & Return flows for depot

--- src/app/depots/depot/depot.component.ts
+++ src/app/depots/depot/depot.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import {DepotEntityService} from '../services/depot-entity.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {map} from 'rxjs/operators';
+import {map, withLatestFrom} from 'rxjs/operators';
 import {combineLatest, Observable} from 'rxjs';
 import {Depot} from '../model/depot';
 import {MessageService} from 'primeng/api';
@@ -15,8 +15,10 @@ import {ConfirmationService} from 'primeng/api';
   styleUrls: ['./depot.component.css']
 })
 export class DepotComponent implements OnInit {
-  @Input() idDepot$: Observable<number>;
-  depot$: Observable<Depot>;
+  @Input() depot: Depot;
+  @Output() onUpdate = new EventEmitter<Depot>();
+  @Output() onClose = new EventEmitter();
+
   constructor(
       private depotsService: DepotEntityService,
       private route: ActivatedRoute,
@@ -28,20 +30,19 @@ export class DepotComponent implements OnInit {
   ngOnInit(): void {
     // comment: this component is sometimes called from his parent Component with idDepot @Input Decorator,
     // or sometimes via a router link via the Main Menu
-    if (!this.idDepot$) {
-      // we must come from the menu
-      console.log('We initialize a new depot object from the router!');
-      this.idDepot$ = this.route.paramMap
-          .pipe(
-              map(paramMap => paramMap.get('idDepot')),
-              map(idDepotString => Number(idDepotString))
-          );
-    }
 
-    this.depot$ = combineLatest([this.idDepot$, this.depotsService.entities$])
+    if (!this.depot) {
+      // we must come from the menu
+      console.log('We initialize a new user object from the router!');
+      this.route.paramMap
         .pipe(
-            map(([idDepot, depots]) => depots.find(depot => depot['idDepot'] === idDepot.toString()))
-        );
+            map(paramMap => paramMap.get('idDepot')),
+            withLatestFrom(this.depotsService.entities$),
+            map(([idDepot, depots]) => depots.find(depot => depot.idDepot === idDepot))
+        )
+        .subscribe(depot => this.depot = depot);
+        
+    }
   }
 
   save(oldDepot: Depot, depotForm: Depot) {
@@ -52,6 +53,8 @@ export class DepotComponent implements OnInit {
           // Emanuel if we did not come from the router,
           // post ( via an @Output event ? the displayDialog property of its parent depots.component to false so we are done
           console.log('We hide the depot component');
+          this.onUpdate.emit(modifiedDepot);
+          this.onClose.emit();
     });
   }
 
@@ -66,10 +69,11 @@ export class DepotComponent implements OnInit {
          // Emanuel if we did not come from the router,
          // post ( via an @Output event ? the displayDialog property of its parent depots.component to false so we are done
           console.log('We hide the depot component');
+          this.onClose.emit();
         },
         reject: () => {
          console.log('We do nothing');
         }
       });
-    }
+  }
 }
