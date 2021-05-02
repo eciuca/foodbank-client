85e8c4ae714ed483f61f4cf9812aa8f63d960b50
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-05-02 09:45:01 +0300

    Add dockerfile and apache container configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
+<<<<<<< HEAD
   apiUrl: 'http://food-it-server:8081/api/'
+=======
+  apiUrl: 'http://34.107.67.3:8081/api/'
+>>>>>>> 5514caf (Add dockerfile and apache container configuration)
 };
