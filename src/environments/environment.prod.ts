f41b20acf9ccdc4c3dfc0ee9f8e6db0b12c41f49
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-05-04 11:54:30 +0300

    Update prod apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   apiUrl: 'http://food-it-server:8081/api/'
 =======
   apiUrl: 'http://34.107.67.3:8081/api/'
@@ -9,4 +10,7 @@ export const environment = {
 =======
   apiUrl: 'http://34.107.67.34:8081/api/'
 >>>>>>> d7a3b34 (Update prod apiUrl)
+=======
+  apiUrl: 'http://34.89.187.24:8081/api/'
+>>>>>>> 58b2717 (Update prod apiUrl)
 };
