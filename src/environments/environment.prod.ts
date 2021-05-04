00ae74b4c66e5dd281784d3c657d4bbabe57c095
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-05-04 09:54:23 +0300

    Update prod apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,12 @@
 export const environment = {
   production: true,
 <<<<<<< HEAD
+<<<<<<< HEAD
   apiUrl: 'http://food-it-server:8081/api/'
 =======
   apiUrl: 'http://34.107.67.3:8081/api/'
 >>>>>>> 5514caf (Add dockerfile and apache container configuration)
+=======
+  apiUrl: 'http://34.107.67.34:8081/api/'
+>>>>>>> d7a3b34 (Update prod apiUrl)
 };
