ff307a61787d18bd125c9480ede589d4074154dd
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-06-02 17:13:12 +0300

    Update auth-module-config

--- src/app/auth/auth-module-config.ts
+++ src/app/auth/auth-module-config.ts
@@ -2,7 +2,8 @@ import { OAuthModuleConfig } from 'angular-oauth2-oidc';
 
 export const authModuleConfig: OAuthModuleConfig = {
   resourceServer: {
-    allowedUrls: ['http://localhost:4200/api', 'http://localhost:8081/api'],
+    // allowedUrls: ['http://localhost:4200/api', 'http://localhost:8081/api'],
+    allowedUrls: ['https://dev.foodbankit.org/api'],
     sendAccessToken: true,
   }
 };
