2bfd4d1b45587bccaf40f9a6aea5ee1f95d472a3
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-06-02 12:52:05 +0300

    Update environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://dev.foodbankit.org/api/',
-  authServerIssuer: 'https://auth.foodbankit.org/auth/realms/FoodBank'
+  authServerIssuer: 'https://auth.dev.foodbankit.org/auth/realms/FoodBank'
 };
