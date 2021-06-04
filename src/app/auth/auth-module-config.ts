15358f1b1ef25f06672a8de0e852dc2f7a42c8e1
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-06-04 09:11:37 +0300

    Fix auth module config

--- src/app/auth/auth-module-config.ts
+++ src/app/auth/auth-module-config.ts
@@ -1,9 +1,9 @@
 import { OAuthModuleConfig } from 'angular-oauth2-oidc';
+import { environment } from '../../environments/environment';
 
 export const authModuleConfig: OAuthModuleConfig = {
   resourceServer: {
-    // allowedUrls: ['http://localhost:4200/api', 'http://localhost:8081/api'],
-    allowedUrls: ['https://dev.foodbankit.org/api'],
+    allowedUrls: [ environment.apiUrl ],
     sendAccessToken: true,
   }
 };
