2555cf06af0c4dcbfa135e7c9879702053cf8d0f
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-06-07 20:35:27 +0300

    Fix filter base init + login race condition

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import {filter, map, mergeMap} from 'rxjs/operators';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {filter, mergeMap} from 'rxjs/operators';
+import {BehaviorSubject} from 'rxjs';
 import {User} from './model/user';
 import {UserEntityService} from './services/user-entity.service';
 import {Router} from '@angular/router';
-import {globalAuthState} from '../auth/auth.selectors';
+import {globalAuthState, isLoggedIn} from '../auth/auth.selectors';
 import {select, Store} from '@ngrx/store';
 import {AppState} from '../reducers';
 import {LazyLoadEvent} from 'primeng/api';
 import {enmUserRolesAsso, enmUserRolesBankAsso, enmLanguageLegacy } from '../shared/enums';
 import {OrganisationEntityService} from '../organisations/services/organisation-entity.service';
 import {QueryParams} from '@ngrx/data';
-import {Organisation} from '../organisations/model/organisation';
 
 
 @Component({
@@ -84,40 +83,42 @@ export class UsersComponent implements OnInit {
   reload() {
       this.loading = true;
       this.totalRecords = 0;
+
       this.store
         .pipe(
             select(globalAuthState),
-            map((authState) => {
-                console.log('Entering Users component with authState:',authState);
-              if (authState.banque) {
-                  this.bankid = authState.banque.bankId;
-                  this.bankName = authState.banque.bankName;
-                  switch (authState.user.rights) {
-                      case 'Bank':
-                      case 'Admin_Banq':
-                          this.booShowOrganisations = true;
-                          this.filterBase = {'lienBanque': authState.banque.bankId};
-                          this.rightOptions = enmUserRolesBankAsso;
-                          if (authState.user.rights === 'Admin_Banq') {
-                              this.booCanCreate = true;
-                          }
-                          break;
-                      case 'Asso':
-                      case 'Admin_Asso':
-                          this.filterBase = {'idOrg': authState.organisation.idDis};
-                          this.orgName = authState.organisation.societe;
-                          this.rightOptions = enmUserRolesAsso;
-                          if (authState.user.rights === 'Admin_Asso') {
-                              this.booCanCreate = true;
-                          }
-                          break;
-                      default:
-                          console.log('Entering Users component with unsupported user rights, see complete authstate:', authState);
-                  }
-              }
-                console.log('Users FilterBase is: ', this.filterBase );
-             })
-    ).subscribe();
+            filter(authState => authState.isLoggedIn)
+        ).subscribe((authState) => {
+            console.log('Entering Users component with authState:', authState);
+            if (authState.banque) {
+                this.bankid = authState.banque.bankId;
+                this.bankName = authState.banque.bankName;
+                switch (authState.user.rights) {
+                    case 'Bank':
+                    case 'Admin_Banq':
+                        this.booShowOrganisations = true;
+                        this.filterBase = {'lienBanque': authState.banque.bankId};
+                        this.rightOptions = enmUserRolesBankAsso;
+                        if (authState.user.rights === 'Admin_Banq') {
+                            this.booCanCreate = true;
+                        }
+                        break;
+                    case 'Asso':
+                    case 'Admin_Asso':
+                        this.filterBase = {'idOrg': authState.organisation.idDis};
+                        this.orgName = authState.organisation.societe;
+                        this.rightOptions = enmUserRolesAsso;
+                        if (authState.user.rights === 'Admin_Asso') {
+                            this.booCanCreate = true;
+                        }
+                        break;
+                    default:
+                        console.log('Entering Users component with unsupported user rights, see complete authstate:', authState);
+                }
+            }
+            
+            console.log('Users FilterBase is: ', this.filterBase);
+        });
   }
   handleSelect(user) {
     console.log( 'User was selected', user);
@@ -155,38 +156,46 @@ export class UsersComponent implements OnInit {
   }
 
  nextPage(event: LazyLoadEvent) {
-     console.log('Lazy Loaded Event', event, 'FilterBase:', this.filterBase);
-      this.loading = true;
-      const queryParms = {...this.filterBase};
-      queryParms['offset'] = event.first.toString();
-      queryParms['rows'] = event.rows.toString();
-      queryParms['sortOrder'] = event.sortOrder.toString();
-      if (event.sortField) {
-          queryParms['sortField'] = event.sortField.toString();
-      } else {
-          queryParms['sortField'] =  'idUser';
-      }
-          if (this.booShowOrganisations && this.filteredOrganisation && this.filteredOrganisation.idDis != null) {
-         queryParms['idOrg'] = this.filteredOrganisation.idDis;
-     }
-      if (event.filters) {
-          if (event.filters.idUser && event.filters.idUser.value) {
-              queryParms['idUser'] =  event.filters.idUser.value;
-          }
-          if (event.filters.userName && event.filters.userName.value) {
-              queryParms['userName'] = event.filters.userName.value;
-          }
-          if (event.filters.idLanguage && event.filters.idLanguage.value) {
-                queryParms['idLanguage'] = event.filters.idLanguage.value;
-          }
-          if (event.filters.email && event.filters.email.value) {
-                queryParms['email'] = event.filters.email.value;
-          }
-          if (event.filters.rights && event.filters.rights.value) {
-                queryParms['rights'] = event.filters.rights.value;
-          }
-      }
-     this.loadPageSubject$.next(queryParms);
+      this.store
+        .pipe(
+            select(isLoggedIn),
+            filter(isLoggedIn => isLoggedIn))
+        .subscribe(_ => {
+            console.log('Lazy Loaded Event', event, 'FilterBase:', this.filterBase);
+            this.loading = true;
+            const queryParms = {...this.filterBase};
+            queryParms['offset'] = event.first.toString();
+            queryParms['rows'] = event.rows.toString();
+            queryParms['sortOrder'] = event.sortOrder.toString();
+            if (event.sortField) {
+                queryParms['sortField'] = event.sortField.toString();
+            } else {
+                queryParms['sortField'] =  'idUser';
+            }
+                if (this.booShowOrganisations && this.filteredOrganisation && this.filteredOrganisation.idDis != null) {
+                queryParms['idOrg'] = this.filteredOrganisation.idDis;
+            }
+            if (event.filters) {
+                if (event.filters.idUser && event.filters.idUser.value) {
+                    queryParms['idUser'] =  event.filters.idUser.value;
+                }
+                if (event.filters.userName && event.filters.userName.value) {
+                    queryParms['userName'] = event.filters.userName.value;
+                }
+                if (event.filters.idLanguage && event.filters.idLanguage.value) {
+                        queryParms['idLanguage'] = event.filters.idLanguage.value;
+                }
+                if (event.filters.email && event.filters.email.value) {
+                        queryParms['email'] = event.filters.email.value;
+                }
+                if (event.filters.rights && event.filters.rights.value) {
+                        queryParms['rights'] = event.filters.rights.value;
+                }
+            }
+
+            this.loadPageSubject$.next(queryParms)
+        })
+     ;
   }
 
     filterOrganisation(event ) {
