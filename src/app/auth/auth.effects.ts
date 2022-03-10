d9bd95fc8f2a247419c4b39bff2192aeb27eeabb
Alain Vandermeersch <alain.vandermeersch@gmail.com>
2022-03-10 14:15:56 +0100

    remove auth logoff effect that reroutes to home page and inhibits proper logoff

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -25,7 +25,7 @@ export class AuthEffects {
                 ofType(AuthActions.logout),
                 tap(action => {
                     localStorage.removeItem('user');
-                    this.router.navigateByUrl('/home');
+                   // this.router.navigateByUrl('/home');
                 })
             )
     , {dispatch: false});
