9e77611ccf82d9a5877d9bd742bb6244032db59b
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2022-06-13 17:56:19 +0200

    Create separate configs for ovh dev and prod

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://dev.foodbanksit.be/api/',
+  authServerIssuer: 'https://auth.dev.foodbanksit.be/auth/realms/FoodBank',
+  availableLocales: [ 'en-US', 'fr-FR', 'nl-NL' ]
+};
